7199ba12f47411ad99829a4fd211252a4f6a9118
AudreyHal <chiomahalim@yahoo.com>
2019-06-09 23:13:01 +0100

    Component creation

---
+++ src/components/landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="Landing">
+         
+     </div>
+       
+    );
+  }
+}
+
+export default Landing;
