01e20a52dcf51f33fa88d7961881d8e2b9338ad8
AudreyHal <chiomahalim@yahoo.com>
2019-06-16 22:09:08 +0100

    Conditional rendering of components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Landing from './components/landing';
+import CaptainAmerica from './components/captainAmerica';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {displayComponent: "Landing"};
+  }
+
+ clicked(clicked_item){
+  this.setState({ displayComponent: clicked_item})
+ }
+
   render() {
+  //  const landing =this.state.landing
+  //  const  captainAmerica =this.state.captainAmerica
+  const displayComponent = this.state.displayComponent
     return (
       <div className="App">
-        {/* <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div> */}
-        <Landing></Landing>
+        {displayComponent === "Landing"  && <Landing clicked={this.clicked.bind(this)}></Landing>}
+        {displayComponent === "CaptainAmerica" && <CaptainAmerica></CaptainAmerica>}
       </div>
     );
   }
