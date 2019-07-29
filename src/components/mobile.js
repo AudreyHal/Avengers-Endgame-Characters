b3e53f3dcb6abc21c03c352ea0380a47876f8fb5
chioma <chioma.h@ng.lopworks.com>
2019-07-29 16:03:19 +0100

    change app design

--- src/components/mobile.js
+++ src/components/mobile.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-// import Stark from '../images/stark_mobile3.jpg';
-import America from '../images/america_mobile.jpg';
+import Stark from '../images/stark_mobile3.jpg';
+// import America from '../images/america_mobile.jpg';
+import America from '../images/america.svg';
 import Natasha from '../images/nat_mobile.jpg';
-import data from '../data/data.json'
+import Natasha2 from '../images/natasha.svg';
+// import Natasha2 from '../images/natasha.svg';
+import data from '../data/data.json';
+import logo from '../images/avengers_logo_3.png';
 
 class Mobile extends Component {
   constructor(){
     super();
     this.state ={
-      imgArray2: [America, Natasha ],
-      // imgArray: [antMan, america, hawkEye, hulk, marvel, nebula, okoye, racoon, stark, thanos, thor, warMachine, widow, svg ], 
+      imgArray2: [America, Natasha2, Stark, America, Natasha ],
       active: "false",
       background: America,
       arrayIndex:0,
@@ -27,40 +30,37 @@ class Mobile extends Component {
   render() {
     return (
       <div className="Mobile" style={{backgroundImage: `url(${this.state.imgArray2[this.state.arrayIndex]})`}}>
-
-<nav id="sidebar" className={this.state.active && 'active'}>
-  <div id="close" onClick={() => this.setState({active: !this.state.active})}>X</div>
-    <div className="sidebar-header">
-       
+        <nav id="sidebar" className={this.state.active && 'active'}>
+          <div id="close" onClick={() => this.setState({active: !this.state.active})}>X</div>
+            <div className="sidebar-header">
+          </div>
+          <ul className="nav_list">
+              <li>
+                  <a onClick={e=> this.clickedCharacter(e, "Stark", 0)}>Iron Man</a>
+              </li>
+              <li>
+                <a onClick={e=> this.clickedCharacter(e, "Stark", 0)}>Captain America</a>
+              </li>
+              <li>
+                <a onClick={e=> this.clickedCharacter(e, "Stark", 0)}>Captain America</a>
+              </li>
+              <li>
+                <a onClick={e=> this.clickedCharacter(e, "Stark", 0)}>Captain America</a>
+              </li>
+          </ul>
+        </nav>
+      <div id="content">
+      <div className="mobile-header">
+        <img src={logo} alt="avengers_endgame_logo" width="100px"></img>
       </div>
-
-        <ul className="nav_list">
-            <li>
-                <a onClick={e=> this.clickedCharacter(e, "Stark", 0)}>Iron Man</a>
-            </li>
-            <li>
-                <a >Captain America</a>
-            </li>
-            <li>
-                <a >Black Widow</a>
-            </li>
-            <li>
-                <a >Contact</a>
-            </li>
-        </ul>
-    </nav>
-    <div id="content">
-
-        
-
         <div className="mobile-container">
-          <button onClick={() => this.setState({active: !this.state.active})}>aadd</button>
-        <div className="col-12 mobile-text-container ">
+        <div className="col-12 mobile-text-container">
           <div className="dark-overlay">
             <p className="mobile-name">Tony Stark</p>
             <p className="mobile-title">Iron Man</p>
-            <p className="mobile-description">Genius. Billionaire. Playboy. Philanthropist. Tony Stark's confidence is only matched by his high-flying abilities as the hero called Iron Man.</p></div>
-          
+            <p className="mobile-description">Genius. Billionaire. Playboy. Philanthropist. Tony Stark's confidence is only matched by his high-flying abilities as the hero called Iron Man.</p>
+          <div className="text-center"><button className="btn character-button" onClick={() => this.setState({active: !this.state.active})}>Change Character</button></div>
+          </div>
         </div>
         </div>
 
